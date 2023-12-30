72fb08afbf1d208e57af6892f25003915ae82129
Nayron <nayronmoura@gmail.com>
2023-12-30 17:13:12 -0300

    chore: remove url parser

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:portifolio/app_module.dart';
 import 'package:portifolio/app_widget.dart';
-import 'package:portifolio/configure_web.dart';
 
 void main() async {
-  configureApp();
   runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
