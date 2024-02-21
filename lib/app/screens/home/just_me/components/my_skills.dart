994add08e9ea9bc8d9b9012e67a5246e62e490df
Nayron <nayronmoura@gmail.com>
2024-02-21 15:02:40 -0300

    fix: images

--- lib/app/screens/home/just_me/components/my_skills.dart
+++ lib/app/screens/home/just_me/components/my_skills.dart
@@ -35,25 +35,25 @@ class MySkills extends StatelessWidget {
           const SizedBox(height: 10),
           const Wrap(children: [
             LanguageCard(
-              imageUrl: 'svg/flutter.svg',
+              imageUrl: 'assets/svg/flutter.svg',
               title: 'Flutter',
               description:
                   'Flutter is Google’s UI toolkit for building beautiful, natively compiled applications for mobile, web, and desktop from a single codebase.',
             ),
             LanguageCard(
-              imageUrl: 'svg/java.svg',
+              imageUrl: 'assets/svg/java.svg',
               title: 'Java',
               description:
                   'Java is a general-purpose, class-based, object-oriented programming language designed for having lesser implementation dependencies. It is a concurrent language and a compiled language. It is a statically typed and reflective language.',
             ),
             LanguageCard(
-              imageUrl: 'svg/spring.svg',
+              imageUrl: 'assets/svg/spring.svg',
               title: 'Spring',
               description:
                   'Spring is an open-source application framework and inversion of control container for the Java platform. The Spring Framework provides a comprehensive infrastructure for developing Java applications.',
             ),
             LanguageCard(
-              imageUrl: 'svg/postgres.svg',
+              imageUrl: 'assets/svg/postgres.svg',
               title: 'Postgres',
               description:
                   'Postgres (also known as PostgreSQL) is an open-source object-relational database management system (RDBMS) that emphasizes extensibility and standards compliance. It is one of the most popular RDBMSs in the world, and is used by millions of users and organizations.',
